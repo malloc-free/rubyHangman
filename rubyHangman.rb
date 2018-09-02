f176b2666e713dc44fb89262c2ba69a07993a8ea
Michael Holmwood <mholmwood@gmail.com>
2018-09-02 18:01:45 +1200

    Created Hangman class

--- rubyHangman.rb
+++ rubyHangman.rb
@@ -1,182 +1,190 @@
-class Hangman
-	@iteration = 0
-	@letterCount = 0
-	@target = "test"
-	@noose = ""
-end
 
-if __FILE__ == $0
-	#For ASCII hangman.
-	hangman =  ["\n"]
-	hangman[0] <<  "\n"
-	hangman[0] <<  "\n"
-	hangman[0] <<  "\n"
-	hangman[0] <<  "\n"
-	hangman[0] <<  "\n"
-	
-	hangman[1] =  "\n"
-	hangman[1] <<  "\n"
-	hangman[1] <<  "\n"
-	hangman[1] <<  "\n"
-	hangman[1] <<  "\n"
-	hangman[1] <<  "\n"
-	hangman[1] <<  "|_____________\n"	
-	
-	hangman[2] =  "\n"
-	hangman[2] <<  "|\n"
-	hangman[2] <<  "|\n"
-	hangman[2] <<  "|\n"
-	hangman[2] <<  "|\n"
-	hangman[2] <<  "|\n"
-	hangman[2] <<  "|_____________\n"
-	
-	hangman[3] =  "______________\n"
-	hangman[3] <<  "|\n"
-	hangman[3] <<  "|\n"
-	hangman[3] <<  "|\n"
-	hangman[3] <<  "|\n"
-	hangman[3] <<  "|\n"
-	hangman[3] <<  "|_____________\n"
-	
-	hangman[4] =  "______________\n"
-	hangman[4] <<  "|            |\n"
-	hangman[4] <<  "|            0\n"
-	hangman[4] <<  "|\n"
-	hangman[4] <<  "|\n"
-	hangman[4] <<  "|\n"
-	hangman[4] <<  "|_____________\n"
-	
-	hangman[5] =  "______________\n"
-	hangman[5] <<  "|            |\n"
-	hangman[5] <<  "|            0\n"
-	hangman[5] <<  "|            |\n"
-	hangman[5] <<  "|\n"
-	hangman[5] <<  "|\n"
-	hangman[5] <<  "|_____________\n"
-	
-	hangman[6] =  "______________\n"
-	hangman[6] <<  "|            |\n"
-	hangman[6] <<  "|            0\n"
-	hangman[6] <<  "|            |\\\n"
-	hangman[6] <<  "|\n"
-	hangman[6] <<  "|\n"
-	hangman[6] <<  "|_____________\n"
-	
-	hangman[7] =  "______________\n"
-	hangman[7] <<  "|            |\n"
-	hangman[7] <<  "|            0\n"
-	hangman[7] <<  "|           /|\\\n"
-	hangman[7] <<  "|\n"
-	hangman[7] <<  "|\n"
-	hangman[7] <<  "|_____________\n"
-	
-	hangman[8] =  "______________\n"
-	hangman[8] <<  "|            |\n"
-	hangman[8] <<  "|            0\n"
-	hangman[8] <<  "|           /|\\\n"
-	hangman[8] <<  "|             \\\n"
-	hangman[8] <<  "|\n"
-	hangman[8] <<  "|_____________\n"
-	
-	hangman[9] =  "______________\n"
-	hangman[9] <<  "|            |\n"
-	hangman[9] <<  "|            0\n"
-	hangman[9] <<  "|           /|\\\n"
-	hangman[9] <<  "|           / \\\n"
-	hangman[9] <<  "|\n"
-	hangman[9] <<  "|_____________\n"
-	
-	#So we can keep track of selected letters.
-	alpha = {"a" => false, "b" => false, "c" => false, "d" => false, "e" => false,
+class Hangman
+	def initialize(guessWord)
+		@guessWord = guessWord
+		@letterCount = 0
+		@targetWord = ""
+		@iteration = 0
+		@wordSize = 0
+		@quit = false
+		@alpha = {"a" => false, "b" => false, "c" => false, "d" => false, "e" => false,
 			 "f" => false, "g" => false, "h" => false, "i" => false, "j" => false,
 			 "k" => false, "l" => false, "m" => false, "n" => false, "o" => false, 
 			 "p" => false, "q" => false, "r" => false, "s" => false, "t" => false, 
 			 "u" => false, "v" => false, "w" => false, "x" => false, "y" => false, 
 			 "z" => false}
-	
-	#Quit when this true
-	quit = false
-	#This is used to display current word guesses.
-	#noose = String.new("")
-	#The test word until we have some other mechanisim
-	#@myStr = String.new("test")
-	#Add blanks for letters.
-	#noose << "_" while noose.length < myStr.length
-	
-	#Test string
-	myStr2 = String.new("test this")
-	#Put
-	noose2 = ""
-	count2 = 0
-	myStr2.each_char{|c|
-		if c == " "
-			noose2 << " "
-		else
-			noose2 << "_"
-			count2 += 1
-		end
-	}
-	puts "noose2 = #{noose2}\n"
-	iteration = 0
-	letterCount = 0
-	
-	#loop intil we want to quit
-	until quit
-		#display data
-		puts hangman[iteration]
-		puts noose2
-		puts "iteration = #{iteration}"
-		input = gets.chomp
-		puts "Entered #{input}"
-	
-		#test input
-		#if quitting then bail
-		if input == "quit"
-			quit = true
-		#else check a letter has been input from the hash
-		elsif alpha.has_key?(input)
-			#If the letter has been tried already
-			if alpha[input]
-				puts "letter #{input} already tried"
-			#The letter has not been checked already, so mark it and test the string
+		@guessWord.each_char{|c|
+			if c == " "
+				@targetWord << " "
 			else
-				alpha[input] = true
-				oldval = myStr2.index(input)
-				#If we don't have a match, then mark it as such. 
-				#Then test for game over.
-				if oldval == nil
-					iteration += 1
-					#quit if we have a hangman.
-					if iteration == hangman.size - 1
-						puts "game over"
-						quit = true
-						puts hangman[hangman.size - 1]
-						puts myStr2
-					end
-				#We have a match. Test string until we have all instances of letter.
-				else
-					while oldval != nil
-						letterCount += 1
-						puts "letterCount = #{letterCount}"
-						puts "index = #{oldval}"
-						noose2[oldval] = input
-						#If we have the correct word size, then the game is over.
-						if letterCount == count2
-							puts "Game over, corrent!"
-							quit = true
-						#Test the string for more instances.
-						else 
-							oldval = myStr2.index(input, oldval + 1)
-						end
-					end
-				end
+				@targetWord << "_"
+				@wordSize += 1
 			end
-		else
-		#Error message for incorrect input.
-			puts "please enter a letter"
+		}
+	end
+		#attr_accessor :alpha
+		#attr_accessor :guessWord
+		#attr_accessor :targetWord
+		#attr_accessor :wordSize
+		#attr_accessor :letterCount
+		#attr_accessor :iteration
+		#attr_accessor :quit
+	
+	def start()
+		until @quit
+			#display data
+			puts $hangman[@iteration]
+			puts @targetWord
+			input = gets.chomp
+			puts "Entered #{input}"
+	
+			#test input
+			#if quitting then bail
+			if input == "quit"
+				@quit = true
+			#else check a letter has been input from the hash
+			elsif @alpha.has_key?(input)
+				testLetter input
+			else
+			#Error message for incorrect input.
+				puts "please enter a letter"
+			end
 		end
 	end
-		
+	
+	def testLetter(input)
+		#If the letter has been tried already
+		if @alpha[input]
+			puts "letter #{input} already tried"
+		#The letter has not been checked already, so mark it and test the string
+		else
+			@alpha[input] = true
+			atIndex = @guessWord.index(input)
+			#If we don't have a match, then mark it as such. 
+			#Then test for game over.
+			if atIndex == nil
+				indexNil
+			#We have a match. Test string until we have all instances of letter.
+			else
+				scanWord input, atIndex
+			end
+		end
+	end
+	
+	def indexNil
+		@iteration += 1
+		#quit if we have a hangman.
+		if @iteration == $hangman.size - 1
+			puts "Game Over, hangman complete"
+			@quit = true
+			puts $hangman[$hangman.size - 1]
+			puts @guessWord
+		end
+	end
+	def scanWord(input, atIndex)
+		while atIndex != nil
+			@letterCount += 1
+			@targetWord[atIndex] = input
+			#If we have the correct word size, then the game is over.
+			if @letterCount == @wordSize
+				puts "Game Over, correct phrase!"
+				@quit = true
+			#Test the string for more instances.
+			else 
+				atIndex = @guessWord.index(input, atIndex + 1)
+			end
+		end
+	end
+end
+
+
+	#For ASCII hangman.
+
+BEGIN {
+	$hangman =  ["\n"]
+	$hangman[0] <<  "\n"
+	$hangman[0] <<  "\n"
+	$hangman[0] <<  "\n"
+	$hangman[0] <<  "\n"
+	$hangman[0] <<  "\n"
+	
+	$hangman[1] =  "\n"
+	$hangman[1] <<  "\n"
+	$hangman[1] <<  "\n"
+	$hangman[1] <<  "\n"
+	$hangman[1] <<  "\n"
+	$hangman[1] <<  "\n"
+	$hangman[1] <<  "|_____________\n"	
+	
+	$hangman[2] =  "\n"
+	$hangman[2] <<  "|\n"
+	$hangman[2] <<  "|\n"
+	$hangman[2] <<  "|\n"
+	$hangman[2] <<  "|\n"
+	$hangman[2] <<  "|\n"
+	$hangman[2] <<  "|_____________\n"
+	
+	$hangman[3] =  "______________\n"
+	$hangman[3] <<  "|\n"
+	$hangman[3] <<  "|\n"
+	$hangman[3] <<  "|\n"
+	$hangman[3] <<  "|\n"
+	$hangman[3] <<  "|\n"
+	$hangman[3] <<  "|_____________\n"
+	
+	$hangman[4] =  "______________\n"
+	$hangman[4] <<  "|            |\n"
+	$hangman[4] <<  "|            0\n"
+	$hangman[4] <<  "|\n"
+	$hangman[4] <<  "|\n"
+	$hangman[4] <<  "|\n"
+	$hangman[4] <<  "|_____________\n"
+	
+	$hangman[5] =  "______________\n"
+	$hangman[5] <<  "|            |\n"
+	$hangman[5] <<  "|            0\n"
+	$hangman[5] <<  "|            |\n"
+	$hangman[5] <<  "|\n"
+	$hangman[5] <<  "|\n"
+	$hangman[5] <<  "|_____________\n"
+	
+	$hangman[6] =  "______________\n"
+	$hangman[6] <<  "|            |\n"
+	$hangman[6] <<  "|            0\n"
+	$hangman[6] <<  "|            |\\\n"
+	$hangman[6] <<  "|\n"
+	$hangman[6] <<  "|\n"
+	$hangman[6] <<  "|_____________\n"
+	
+	$hangman[7] =  "______________\n"
+	$hangman[7] <<  "|            |\n"
+	$hangman[7] <<  "|            0\n"
+	$hangman[7] <<  "|           /|\\\n"
+	$hangman[7] <<  "|\n"
+	$hangman[7] <<  "|\n"
+	$hangman[7] <<  "|_____________\n"
+	
+	$hangman[8] =  "______________\n"
+	$hangman[8] <<  "|            |\n"
+	$hangman[8] <<  "|            0\n"
+	$hangman[8] <<  "|           /|\\\n"
+	$hangman[8] <<  "|             \\\n"
+	$hangman[8] <<  "|\n"
+	$hangman[8] <<  "|_____________\n"
+	
+	$hangman[9] =  "______________\n"
+	$hangman[9] <<  "|            |\n"
+	$hangman[9] <<  "|            0\n"
+	$hangman[9] <<  "|           /|\\\n"
+	$hangman[9] <<  "|           / \\\n"
+	$hangman[9] <<  "|\n"
+	$hangman[9] <<  "|_____________\n"
+}
+
+if __FILE__ == $0
+	guessWord = "test this"
+	currentGame = Hangman.new guessWord
+	currentGame.start
 	#alpha.each do |key, value|
 	#	puts "key = #{key}, value = #{value}"
 	#end
